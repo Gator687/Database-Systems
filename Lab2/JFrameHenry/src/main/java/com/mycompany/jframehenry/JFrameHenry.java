7f065230baef610868f37f5787a7ec690029aaea
Gator687 <thomas.halligan68@gmail.com>
2023-09-10 18:57:18 -0700

    lab2

---
+++ Lab2/JFrameHenry/src/main/java/com/mycompany/jframehenry/JFrameHenry.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jframehenry;
+
+/**
+ *
+ * @author Thomas
+ */
+public class JFrameHenry {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
