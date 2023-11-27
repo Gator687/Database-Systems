5c48dfdf419947a35d635d6be80273e4f81821bd
Gator687 <thomas.halligan68@gmail.com>
2023-11-26 18:25:19 -0800

    write to sql jframe

---
+++ lab 3/WriteToMySQLJFrame/src/main/java/com/thomas/writetomysqljframe/WriteToMySQLJFrame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.thomas.writetomysqljframe;
+
+/**
+ *
+ * @author Thomas
+ */
+public class WriteToMySQLJFrame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
