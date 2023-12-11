e99748dbe5c77e92d6f010937d1f8d19c73234fa
Gator687 <thomas.halligan68@gmail.com>
2023-12-10 17:36:31 -0800

    gui

---
+++ lab 6/lab6interface/src/main/java/com/thomas/lab6interface/Lab6interface.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.thomas.lab6interface;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Lab6interface {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
